6d4974ab09ecbe74060bf501665408212903cbba
C0rr4do <46971128+C0rr4do@users.noreply.github.com>
2020-02-23 23:39:12 +0100

    mini-commit - xml-start

---
+++ Software/Desktop/XmlHandling/XmlHandler.cs
@@ -0,0 +1,24 @@
+﻿using Rubinator3000.CubeScan.ColorIdentification;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Rubinator3000.XmlHandling {
+
+    static class XmlHandler {
+
+        public void SaveXDocument(string path, XDocument xDocument) {
+
+            if (File.Exists(path)) {
+
+                File.Delete(path);
+            }
+            xDocument.Save(path);
+        }
+
+        public static void SaveReadPositions(List<ReadPosition> readPositions) {
+
+
+        }
+    }
+}
